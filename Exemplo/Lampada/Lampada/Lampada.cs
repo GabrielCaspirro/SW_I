f7971b8bde40cff0bee2c111f268018020e9e64d
Gabriel Caspirro Demarchi <124791810+GabrielCaspirro@users.noreply.github.com>
2024-02-23 12:40:26 -0300

    Add files via upload

---
+++ Exemplo/Lampada/Lampada/Lampada.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lampada
+{
+    public class Lampada
+    {
+        public bool ligada { get; set; }
+        public int potencia { get; set; }
+        public string? cor { get; set; }
+    }
+}
---
+++ Exemplo/Lampada/Lampada/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lampada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Objeto
+            Lampada lampada = new Lampada();
+            Lampada lampada2 = new Lampada();
+
+            //Atributos
+            lampada.cor = "branca";
+            lampada.potencia = 150;
+            lampada.ligada = false;
+
+            lampada2.cor = "verde";
+            lampada2.potencia = 120;
+            lampada2.ligada = true;
+             
+            Console.WriteLine("Essa é a lampada 1!!");
+            Console.WriteLine($"A Lampada é {lampada.cor}, tem a potencia de {lampada.potencia}w. Ela está ligada? R: {lampada.ligada}");
+            Console.WriteLine("Essa é a lampada 2!!");
+            Console.WriteLine($"A Lampada é {lampada2.cor}, tem a potencia de {lampada2.potencia}w. Ela está ligada? R: {lampada2.ligada}");
+        }
+    }
+}
