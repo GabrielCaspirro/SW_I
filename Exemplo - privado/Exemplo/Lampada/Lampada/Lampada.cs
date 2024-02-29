887568d153e4219c4223f382b28ff31bc024df60
Gabriel Caspirro Demarchi <124791810+GabrielCaspirro@users.noreply.github.com>
2024-02-29 12:36:24 -0300

    Add files via upload

---
+++ Exemplo - privado/Exemplo/Lampada/Lampada/Lampada.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lampada
+{
+    public class Lampada
+    {
+        private bool Ligada { get; set; }
+        private int Potencia { get; set; }
+        private string? Cor { get; set; }
+
+        public void Ligar()
+        {
+            Ligada = true;
+        }
+        public void Desligar()
+        {
+            Ligada = false;
+        }
+        public bool EstaLIgada()
+        {
+            return Ligada;
+        }
+
+        public void LampColor(string color)
+        {
+            Cor = color;
+        }
+
+        public string LampC()
+        {
+            return Cor;
+        }
+
+        public int LampP()
+        {
+            return Potencia;
+        }
+
+        public void LampPower(int power)
+        {
+            Potencia = power;
+        }
+    }
+}
---
+++ Exemplo - privado/Exemplo/Lampada/Lampada/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lampada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Objeto
+            Lampada lampada = new Lampada();
+
+            //Método
+            lampada.Ligar();
+            lampada.LampColor("Verde");
+            lampada.LampPower(140);
+            Console.WriteLine($"Tá ligada?: {lampada.EstaLIgada()}\n Potência: {lampada.LampP()}\n Cor:{lampada.LampC()}");
+        }
+    }
+}
