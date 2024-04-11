46890c2746197795c4744819dd95d299a923432f
Gabriel Caspirro Demarchi <124791810+GabrielCaspirro@users.noreply.github.com>
2024-04-11 12:42:48 -0300

    Add files via upload

---
+++ AreaQuadradoRetangulo/Area.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaQuadradoRetangulo
+{
+    public class Area
+    {
+        public float Base { get; set; }
+        public float Altura { get; set; }
+        public float Resultado { get; set; }
+
+        public void Calcular()
+        {
+            Console.WriteLine("Digite o valor da base da figura: ");
+            Base = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor da base da figura: ");
+            Altura = float.Parse(Console.ReadLine());
+
+            Resultado = Base * Altura;
+
+            if(Base != Altura)
+            {
+                Console.WriteLine($"A área é de {Resultado} e a figura é um retângulo.");
+            }
+            else
+            {
+                Console.WriteLine($"A área é de {Resultado} e a figura é um quadrado.");
+            }
+        }
+
+        public string Dados()
+        {
+            return $"Base: {Base} \nAltura: {Altura}";
+        }
+    }
+}
---
+++ AreaQuadradoRetangulo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AreaQuadradoRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Area a = new();
+            a.Calcular();
+            Console.WriteLine(a.Dados());
+        }
+    }
+}
