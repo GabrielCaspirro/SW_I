e5483cce979af3e11cd20b86d74af1060188605b
Gabriel Caspirro Demarchi <124791810+GabrielCaspirro@users.noreply.github.com>
2024-04-25 12:27:55 -0300

    Add files via upload

---
+++ ExemploConstrutor/ExemploConstrutor/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploConstrutor
+{
+    class Cliente
+    {
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public int Senha { get; set; }
+
+        public Cliente(string nome)
+        {
+            Nome = nome;
+        }
+        public void MostraDados()
+        {
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Email: " + Email);
+            Console.WriteLine("Senha" + Senha);
+        }
+    }
+}
---
+++ ExemploConstrutor/ExemploConstrutor/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ExemploConstrutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo Construtor");
+            Cliente cliente = new Cliente("Gabriel");
+            cliente.Email = "GAB@gmail.com";
+            cliente.Senha = 2122344;
+            cliente.MostraDados();
+        }
+    }
+}
